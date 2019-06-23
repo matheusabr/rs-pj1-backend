f54ce798f839970da683d26d64c52fc8e968212a
Matheus Abreu <mabreu@PDM-00870.lan>
2019-06-23 16:50:42 +0100

    task: define a post schema to represent its data on db

---
+++ src/models/Post.js
@@ -0,0 +1,28 @@
+/**
+ * DB table representation
+ */
+const mongoose = require('mongoose')
+
+/**
+ * Using timestamp: true
+ * we enable DB to register:
+ * createdAt and updatedAt
+ */
+const PostSchema = new mongoose.Schema(
+  {
+    author: String,
+    place: String,
+    description: String,
+    hashtags: String,
+    image: String,
+    likes: {
+      type: Number,
+      default: 0
+    }
+  },
+  {
+    timestamp: true
+  }
+)
+
+module.exports = mongoose.model('Post', PostSchema)
