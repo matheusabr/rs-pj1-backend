3355ae47e8ceddc4769c67ec5d0d65c6da244227
Matheus Abreu <matheusabreu.dev@gmail.com>
2019-06-23 21:50:48 +0100

    config: add sharp to handle image resize and set to uploader

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -2,6 +2,9 @@
  * Business logics
  */
 const Post = require('../models/Post')
+const sharp = require('sharp')
+const path = require('path')
+const fs = require('fs')
 
 /**
  * Execute async functions
@@ -18,6 +21,16 @@ module.exports = {
   async store(req, res) {
     const { author, place, description, hashtags } = req.body
     const { filename: image } = req.file
+
+    // Resize image file
+    await sharp(req.file.path)
+      .resize(500)
+      .jpeg({ quality: 70 })
+      .toFile(path.resolve(req.file.destination, 'resized', image))
+
+    // Remove original image file
+    fs.unlinkSync(req.file.path)
+
     // Save to DB
     const post = await Post.create({
       author,
